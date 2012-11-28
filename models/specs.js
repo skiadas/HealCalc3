9e66fa8b4b27b4d62eaa50ae9646ba7b9a5c9100
Haris Skiadas <cskiadas@gmail.com>
2012-11-28 02:44:16 -0500

    Temporarily revert to 1.0.7 until we can resolve the problems the newer version was causing

--- models/specs.js
+++ models/specs.js
@@ -137,12 +137,12 @@ Spec = can.Model({
 
 Spec.List = can.Model.List({
     val_update: function() {
-        can.Observe.startBatch();
+        // can.Observe.startBatch();
         var self = this;
         self.each(function(spec) {
             spec.val_update();
         });
-        can.Observe.stopBatch();
+        // can.Observe.stopBatch();
     }
 });
 // can.fixture("/specs", "fixtures/spec.json");
