12cdd9f0a6be1e4ef1720786a0c6ac466984b749
Haris Skiadas <cskiadas@gmail.com>
2012-11-29 02:40:17 -0500

    First steps towards refining the filtering

--- js/filtering.js
+++ js/filtering.js
@@ -1,17 +1,35 @@
 (function() {
+ALLSPECS = ['disc', 'holy', 'pally', 'druid', 'shaman', 'monk'];
 Filtering = can.Control({
     init: function(element, options) {
         var self = this;
-        self.options.table = options.table;
-        self.options.specs = options.specs;
-        self.options.fullmask = (1 << 10) -1;
-        self.options.mask = self.options.fullmask;
-        var view = options.view;
-        $.when(self.element.append(can.view(view, {}))).then(function() {
-            $('input', self.element).attr('checked', true);
+        self.options = options;
+        // self.options.table = options.table;
+        // self.options.specs = options.specs;
+        self.options.filters = new can.Observe({
+            allspecs: true,
+            disc: true, holy: true, pally: true, druid: true,
+            shaman: true, monk: true, aoe: true, single: true, 
+            instant: true, casttime: true
         });
+        self.options.fullmask = (1 << 10) -1;
+        // self.options.mask = self.options.fullmask;
+        // var view = options.view;
+        self.element.append(can.view(options.view, self.options));
     },
     'input click': function(el, ev) {
+        var filters = this.options.filters;
+        var attr = el.attr('id').split('_')[1];
+        filters.attr(attr, !filters.attr(attr));
+        var onoff = filters.attr(attr);
+        if (attr == 'allspecs') {
+            $.each(ALLSPECS, function(_, sp) {filters.attr(sp, onoff)});
+        }
+    },
+    'change': function() {
+        console.log('A change!')
+    },
+    update_table: function() {
         var input = el.closest('input');
         var settings = $(input).data('filter');
         if (settings.attr == 'allspecs') {
