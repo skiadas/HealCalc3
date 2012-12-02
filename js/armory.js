7cd3206a320ec048fb184cfdc0b3c49806535642
Haris Skiadas <cskiadas@gmail.com>
2012-12-01 22:59:46 -0500

    Add saved searches history in armory

--- js/armory.js
+++ js/armory.js
@@ -1,7 +1,21 @@
 (function() {
+    _from_str = function(str) {
+        var s1 = str.split("@");
+        var s2 = s1[1].split("-");
+        return({'armory_name': s1[0], 'armory_realm': s2[0], 'armory_region': s2[1]});
+    };
+    _to_str = function(character) {
+        character.armory_region = character.armory_region.toUpperCase();
+        return(can.sub("{armory_name}@{armory_realm}-{armory_region}", character));
+    };
     Armory = can.Control({
         init: function(element, options) {
-            this.options.character = new can.Observe({armory_name: '', armory_realm: '', armory_region: 'us'});
+            this.options.character = new can.Observe({armory_name: '', armory_realm: '', armory_region: 'US'});
+            var searches = $.cookie('armory_searches');
+            this.options.past_searches = (searches == null) ? [] : searches.split('&');
+            if (this.options.past_searches.length > 0) {
+                this.options.character.attr(_from_str(this.options.past_searches[0]));
+            }
             this.element = element;
             this.element.html(can.view('views/armory.ejs', this.options));
         },
@@ -14,10 +28,25 @@
         'input[type="button"] click': function(el, ev) {
             this.readArmory();
         },
+        'select change': function(el,ev) {
+            this.options.character.attr(_from_str($(el).val()));
+        },
         readArmory: function() {
             var character = this.options.character;
+            var past_searches = this.options.past_searches;
+            var self=this
             var call = can.sub('cgi-bin/armory.cgi?region={armory_region}&server={armory_realm}&character={armory_name}', character);
             $.getJSON(call, function(json, a, b) { 
+                // Add successful searches to the list
+                var search = _to_str(character);
+                var i = past_searches.indexOf(search);
+                if (i != -1) { past_searches.splice(i, 1); }
+                past_searches.unshift(search);
+                // Keep search list to at most 20
+                if (past_searches.length > 20) {
+                    past_searches.pop();
+                }
+                $.cookie('armory_searches', past_searches.join('&'));
                 var spec = {5: "disc", 2: "pally", 11: "druid", 7: "shaman", 1: "monk"}[json['class']];
                 // Strictly speaking we should look to see if it is really holy
                 // For now, holy priests will need to manually adjust
@@ -29,9 +58,12 @@
                     bhaste: json.stats.hasteRating,
                     bweapon: json.stats.spellPower - json.stats.int + 10
                 };
+                can.Observe.startBatch();
+                $('#filters').data('controls')[0].setSpec(spec);
                 $('div', $('#stats')).data('stat').attr(armory_stats).save();
+                can.Observe.stopBatch();
                 // Change filter to the appropriate spec
-                var all = $('#filters').data('controls')[0].setSpec(spec);
+                self.element.html(can.view('views/armory.ejs', self.options));
             });
         }
     });
