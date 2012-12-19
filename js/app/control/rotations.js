94c9a1496442ede180c84478d6860639730efc88
Haris Skiadas <cskiadas@gmail.com>
2012-12-18 20:24:39 -0500

    Added basic table functionality to rotations.

--- js/app/control/rotations.js
+++ js/app/control/rotations.js
@@ -5,12 +5,21 @@ define(['can', 'jquery', 'app/model/rotation', 'jquerypp/dom/cookie', 'jquerypp/
             self.element = element;
             self.options = options;
             self.options.rotations = new can.Observe.List();
-            self.element.append(can.view('js/app/view/rotations.ejs', {rotations: self.options.rotations}));
+            self.element.append(can.view('js/app/view/rotations.ejs', self.options));
             return;
         },
         '#btn_new_rotation click': function(el, ev) {
-            var spec = this.options.specs[0];
-            this.options.rotations.push(new Rotation(spec));
+            $(el).hide().next().show();
+        },
+        '#btn_create click': function(el, ev) {
+            var val = $(el).siblings('select').val();
+            var nick = $(el).siblings('input[type=input]').val();
+            var spec = this.options.specs[val-1];
+            console.log(spec)
+            console.log(new Rotation(spec, nick))
+            this.options.rotations.push(new Rotation(spec, nick));
+            $('#btn_new_rotation').show();
+            $(el).parent().hide();
         }
     });
     return Rotations;
--- js/app/model/rotation.js
+++ js/app/model/rotation.js
@@ -1,22 +1,23 @@
 define(['can'], function(can) {
     var Rotation = can.Construct({
         init: function(spec, nick) {
-            // this = new can.Observe(this);
             this.nick = nick || 'foo';
             this.spec = spec;
             this.specid = spec.id;
             this.spells = new can.Observe.List([]); // List of objects {no: , spell: }
+            this.results = new can.Observe({});
+            this.val_update();
         },
         _looper : function(fname, delta) {
             var val = 0, spells = this.spells, i;
             for (i=spells.length; i--;) { val += spells[i].no * spells[i][fname].call(spells[i], delta); }
             return val;
         },
-        fheal: function(delta) { return(_looper('fheal', delta)); },
-        fct: function(delta) { return(_looper('fct', delta)); },
-        fmana: function(delta) { return(_looper('fmana', delta)); },
+        fheal: function(delta) { return(this._looper('fheal', delta)); },
+        fct: function(delta) { return(this._looper('fct', delta)); },
+        fmana: function(delta) { return(this._looper('fmana', delta)); },
         val_update: function(delta) {
-            this.attr({
+            this.results.attr({
                 'heal': this.fheal(delta),
                 'ct': this.fct(delta),
                 'mana': this.fmana(delta)
