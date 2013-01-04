9c15d5472bf497f997f66fe39a81fc2dd3227c5e
Haris Skiadas <cskiadas@gmail.com>
2013-01-04 17:03:55 -0600

    Reformatting, spacing out

--- js/app/model/rotation.js
+++ js/app/model/rotation.js
@@ -1,28 +1,41 @@
 define(['can', 'app/model/spec', 'app/model/spell'], function(can, specs, spells) {
     var Rotation = can.Construct({
+
+        // Turns a rotation string into a rotation object
         decode: function(str) {
             var arr = str.split('&'),
                 options = {
                     nick: decodeURIComponent(arr.shift()),
                     spec: specs.find(arr.shift()),
-                    results: {target_time: parseInt(arr.shift().replace('time:',''))},
+                    results: {
+                        target_time: parseInt(arr.shift().replace('time:',''))
+                    },
                     spells: [],
                 },
                 splt, spell,
                 i = arr.length;
+
             while (i--) {
                 splt = arr[i].split(':');
-                options.spells.push({no: splt[0], sp: spells.find(parseInt(splt[1]))});
+                options.spells.push({
+                    no: splt[0],
+                    sp: spells.find(parseInt(splt[1]))
+                });
             }
             return options;
         },
+        
+        // Creates a string from a rotation object
         encode: function(rotation) {
             var arr = [];
+            
             arr.push(encodeURIComponent(rotation.nick));
             arr.push(rotation.spec.name);
             arr.push('time:' + rotation.results.target_time)
-            $.each(rotation.spells, function(_, spell) { arr.push(can.sub('{no}:{sp.id}', spell));});
-            return(arr.join("&"));
+            $.each(rotation.spells, function(_, spell) {
+                arr.push(can.sub('{no}:{sp.id}', spell));
+            });
+            return( arr.join("&") );
         },
         import: function(str) {
             return new this(this.decode(str));
@@ -35,7 +48,8 @@ define(['can', 'app/model/spec', 'app/model/spell'], function(can, specs, spells
             // Each spell is an object { no: , sp: spell }
             this.spells = new can.Observe.List(options.spells || []); 
             this.results = new can.Observe({}); // Contains statistics on the rotation
-            this.results.attr('target_time', (options.results && options.results.target_time) || 120);
+            this.results.attr('target_time', 
+                    (options.results && options.results.target_time) || 120);
             this.val_update();
         },
         contains_not: function(spell) {
@@ -47,8 +61,11 @@ define(['can', 'app/model/spec', 'app/model/spell'], function(can, specs, spells
             } else {
                 var spells = this.spells,
                     i = spells.length;
+
                 while (i--) {
-                    if (spells[i].sp === spell) { return false; }  // spell exists
+                    if (spells[i].sp === spell) {
+                        return false; // spell exists
+                    }
                 }
             }
             return true;
@@ -56,29 +73,59 @@ define(['can', 'app/model/spec', 'app/model/spell'], function(can, specs, spells
         add: function(spell) {
             // Adds the spell to the rotation
             // In the future, this part should decide the count for new spells
-            this.spells.push({no: 1, sp: spell});
+            this.spells.push({
+                no: 1,
+                sp: spell
+            });
             this.val_update();
         },
         export: function() {
             return Rotation.encode(this);
         },
+        // Helper function acting like inject
         _looper : function(fname, delta) {
             var val = 0, spells = this.spells, i;
             for (i=spells.length; i--;) { val += spells[i].no * spells[i].sp[fname].call(spells[i].sp, delta); }
             return val;
         },
-        fheal: function(delta) { return(this._looper('fheal', delta)); },
-        fct: function(delta) { return(this._looper('fct', delta)); },
-        fctp: function(delta) { return(this.fct(delta)/this.results.target_time); },
-        fmana: function(delta) { return(this._looper('fmana', delta)); },
-        fhps: function(delta) { return(this.fheal(delta)/this.fct(delta)); },
-        fehps: function(delta) { return(this.fheal(delta)/this.results.target_time); },
-        fhpm: function(delta) { return(this.fheal(delta)/this.fmana(delta)); },
-        fmps: function(delta) { return(this.fmana(delta)/this.fct(delta)); },
-        femps: function(delta) { return(this.fmana(delta)/this.results.target_time); },
-        fidle: function(delta) { return(this.results.target_time - this.fct(delta)); },
-        fidlep: function(delta) { return(this.fidle(delta)/this.results.target_time); },
-        ftoom: function(delta) {return(this.spec.ftotal_mana(delta)/Math.max(this.femps(delta) - this.spec.fregen_mps(delta), 0));},
+        fheal: function(delta) {
+            return(this._looper('fheal', delta));
+        },
+        fct: function(delta) {
+            return(this._looper('fct', delta));
+        },
+        fctp: function(delta) {
+            return(this.fct(delta) / this.results.target_time);
+        },
+        fmana: function(delta) {
+            return(this._looper('fmana', delta));
+        },
+        fhps: function(delta) {
+            return(this.fheal(delta) / this.fct(delta));
+        },
+        fehps: function(delta) {
+            return(this.fheal(delta ) /this.results.target_time);
+        },
+        fhpm: function(delta) {
+            return(this.fheal(delta) / this.fmana(delta));
+        },
+        fmps: function(delta) {
+            return(this.fmana(delta) / this.fct(delta));
+        },
+        femps: function(delta) {
+            return(this.fmana(delta) / this.results.target_time);
+        },
+        fidle: function(delta) {
+            return(this.results.target_time - this.fct(delta));
+        },
+        fidlep: function(delta) {
+            return(this.fidle(delta) / this.results.target_time);
+        },
+        ftoom: function(delta) {
+            return(this.spec.ftotal_mana(delta) / 
+                    Math.max(this.femps(delta) - this.spec.fregen_mps(delta), 0)
+            );
+        },
         val_update: function(delta) {
             this.results.attr({
                 'heal': Math.roundn(this.fheal(delta)),
--- js/app/model/stat.js
+++ js/app/model/stat.js
@@ -1,25 +1,17 @@
 define(['can'], function(can) {
-var STATS = [
-    {id: 1, name: 'standard', bint: 13400, bspi: 8394, bmast: 3831, bcrit: 2628, bhaste: 555, bweapon: 5812 }
-];
-// 
-// Stat = canm({
-//     findAll: "GET /stats",
-//     findOne: "GET /stat/{id}",
-//     create  : 'POST /stats',
-//     update  : 'PUT /stat/{id}',
-//     destroy : 'DELETE /stat/{id}'
-// },{});
-// 
-// statStore = can.fixture.make(STATS.length, function(i) {
-//     return(STATS[i]);
-// });
-// can.fixture('GET /stats', statStore.findAll);
-// can.fixture('GET /stat/{id}', statStore.findOne);
-// can.fixture('POST /stats', statStore.create);
-// can.fixture('PUT /stat/{id}', statStore.update);
-// can.fixture('DELETE /stat/{id}', statStore.destroy);
-// 
-    var stat = new can.Observe(STATS[0]);
+    var STATS = [
+        {
+            id: 1, 
+            name: 'standard',
+            bint: 13400,
+            bspi: 8394,
+            bmast: 3831,
+            bcrit: 2628,
+            bhaste: 555,
+            bweapon: 5812
+        }
+    ],
+        stat = new can.Observe(STATS[0]);
+
     return stat;
 });
