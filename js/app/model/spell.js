b23427b34ca4d8839e2397ea4b01d3b8b87cf67d
Haris Skiadas <cskiadas@gmail.com>
2013-02-16 13:43:06 -0500

    PW:S is now affected by crit

--- js/app/model/spell.js
+++ js/app/model/spell.js
@@ -1545,7 +1545,8 @@ define(['can'], function(can) {
         fheal: function(delta) {
             return(
                 this.fbase(delta) *
-                ( 1 + 2 * this.spec.fmastp(delta) )
+                ( 1 + 2 * this.spec.fmastp(delta) ) *
+                ( 1 + this.spec.fcritp(delta) )
             );
         },
     });
