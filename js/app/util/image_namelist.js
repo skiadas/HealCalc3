d14e14636dd7da418bdce41ac81ee08a0e337305
Haris Skiadas <cskiadas@gmail.com>
2015-07-27 16:42:58 -0400

    Try to fix weird image bug

--- js/app/util/image_namelist.js
+++ js/app/util/image_namelist.js
@@ -85,6 +85,6 @@ names = [
     'ability_monk_forcesphere',
     'ability_monk_chiwave',
     'ability_monk_chiexplosion',
-    'ability_monk_jadeserpentbreath',
-    'spell_monk_revival'
+    'spell_monk_revival',
+    'ability_monk_jadeserpentbreath'
 ];
